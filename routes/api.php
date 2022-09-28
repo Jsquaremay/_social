603e431ed79a00565ccb2e6ed986da6de48e885c
J-Jacques <jsquaremay007@gmail.com>
2022-09-28 16:34:29 +0100

    last modification

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\AuthController;
 //public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
- 
+
 //protected routes 
 Route::group(['middleware' => ['auth:sanctum']], function(){
 
