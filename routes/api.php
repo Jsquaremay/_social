f748aaca433df6bd05c356da9edb85732727251e
J-Jacques <jsquaremay007@gmail.com>
2022-10-27 20:07:47 +0100

    Ajout des Routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,5 +33,22 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
     //user
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Post
+
+    Route::get('/posts', [PostController::class, 'index']); // all posts
+    Route::post('/posts', [PostController::class, 'store']); // create post
+    Route::get('/posts/{id}', [PostController::class, 'show']); // get single post
+    Route::put('/posts/{id}', [PostController::class, 'update']); //update post 
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']); //delete post
+
+    // Comment
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']); // all comments
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']); // create comment
+    Route::put('/comments/{id}', [CommentController::class, 'update']); // update comment
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']); //delete post
+    
+    //
+    Route::post('/posts/{id}/likes', [LikeController::class, 'likeOrUnlike']); // like or dislike back a post  
 
 });
